4e1b921ee4bc499e7912201a01dbc89cf81b8368
Adrian Soundy <adriansoundy@gmail.com>
2018-06-05 21:03:59 +1200

    Initial version of nanoFramework.Hardware.Esp32 (#1)

---
+++ source/Configuration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Windows.Devices.Gpio;
+
+namespace nanoFramework.Hardware.Esp32
+{
+    /// <summary>
+    /// Change Configuration
+    /// </summary>
+    public class Configuration
+    {
+        /// <summary>
+        /// Set the default function for a GPIO pin
+        /// </summary>
+        /// <remarks>
+        /// Allows gpio pins to be assigned a device function.
+        /// For example setting the I2C1 data pin to use GPIO pin 17.
+        /// </remarks>
+        /// <param name="pin"></param>
+        /// <param name="value"></param>
+        public static void SetPinFunction(int pin, DeviceFunction value)
+        {
+            GpioPin gpioPin = GpioController.GetDefault().OpenPin(pin);
+            gpioPin.SetAlternateFunction((int)value);
+            gpioPin.Dispose();
+        }
+    }
+}
---
+++ source/Logging.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace nanoFramework.Hardware.Esp32
+{
+    /// <summary>
+    /// Controls the logging output.
+    /// By default the logging is LOG_LEVEL_NONE as the same port is used for the Visual Studio debug connection when connected via serial port.
+    /// </summary>
+    class Logging
+    {
+        /// <summary>
+        /// Enumeration of Log levels
+        /// </summary>
+        public enum LogLevel
+        {
+            /// <summary>
+            /// No log output 
+            /// </summary>
+            LOG_LEVEL_NONE,
+            /// <summary>
+            /// Critical errors, software module can not recover on its own 
+            /// </summary>
+            LOG_LEVEL_ERROR,
+            /// <summary>
+            /// Error conditions from which recovery measures have been taken 
+            /// </summary>
+            LOG_LEVEL_WARN,
+            /// <summary>
+            /// Information messages which describe normal flow of events 
+            /// </summary>
+            LOG_LEVEL_INFO,
+            /// <summary>
+            /// Extra information which is not necessary for normal use (values, pointers, sizes, etc). 
+            /// </summary>
+            LOG_LEVEL_DEBUG,
+            /// <summary>
+            /// Bigger chunks of debugging information, or frequent messages which can potentially flood the output. 
+            /// </summary>
+            LOG_LEVEL_VERBOSE
+        }
+
+        /// <summary>
+        /// Set overall logging level.
+        /// </summary>
+        /// <param name="level"></param>
+        public static void SetLogLevel(LogLevel level)
+        {
+            SetLogLevel( "*", level);
+        }
+
+        /// <summary>
+        ///  Set overall logging level for specific tag, tag "*" equals all tags.
+        /// </summary>
+        /// <param name="tag"></param>
+        /// <param name="level"></param>
+        public static void SetLogLevel(string tag, LogLevel level)
+        {
+            NativeSetLogLevel(tag, (int)level);
+        }
+
+        #region native calls
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        private static extern void NativeSetLogLevel(string tag, int level);
+
+        #endregion
+
+    }
+}
