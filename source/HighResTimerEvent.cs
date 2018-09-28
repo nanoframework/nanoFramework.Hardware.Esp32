f6108ee07e99cac43e5981fc69ff6e0835ca78b8
Adrian Soundy <adriansoundy@gmail.com>
2018-09-29 11:34:13 +1200

    High res timer (#15)

---
+++ source/HighResTimerEvent.cs
@@ -0,0 +1,24 @@
+﻿//
+// Copyright (c) 2018 The nanoFramework project contributors
+// See LICENSE file in the project root for full license information.
+//
+
+using System;
+using nanoFramework.Runtime.Events;
+
+
+namespace nanoFramework.Hardware.Esp32
+{
+    [Flags]
+    internal enum HighResTimerEventType : byte
+    {
+        TimerExpired = 101             // HighRes Timer expired event
+    }
+
+    internal class HighResTimerEvent : BaseEvent
+    {
+        public HighResTimerEventType EventType;
+        public uint TimerHandle;
+
+    }
+}
