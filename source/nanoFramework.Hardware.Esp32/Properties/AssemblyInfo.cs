6e0c88e68339280910d988f01eb1d6469986d357
José Simões <jose.simoes@eclo.solutions>
2019-06-22 18:01:04 +0100

    Work on native assembly declaration (#67)

--- source/nanoFramework.Hardware.Esp32/Properties/AssemblyInfo.cs
+++ source/nanoFramework.Hardware.Esp32/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Runtime.InteropServices;
 
 ////////////////////////////////////////////////////////////////
 // update this whenever the native assembly signature changes //
-[assembly: AssemblyNativeVersion("1.0.6.0")]
+[assembly: AssemblyNativeVersion("100.0.6.0")]
 ////////////////////////////////////////////////////////////////
 
 // Setting ComVisible to false makes the types in this assembly not visible 
