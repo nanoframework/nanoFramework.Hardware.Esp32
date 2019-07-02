0dc3081757bb2ea62e5994bd6791e9e61b51e084
Adrian Soundy <adriansoundy@gmail.com>
2019-07-03 05:40:26 +1200

    Update pin mapping and fixes (#70)

--- source/nanoFramework.Hardware.Esp32/Configuration.cs
+++ source/nanoFramework.Hardware.Esp32/Configuration.cs
@@ -4,7 +4,7 @@
 //
 
 using System;
-using Windows.Devices.Gpio;
+using System.Runtime.CompilerServices;
 
 namespace nanoFramework.Hardware.Esp32
 {
@@ -14,19 +14,36 @@ namespace nanoFramework.Hardware.Esp32
     public class Configuration
     {
         /// <summary>
-        /// Set the default function for a GPIO pin
+        /// Set the default function for a GPIO pin.
         /// </summary>
         /// <remarks>
         /// Allows gpio pins to be assigned a device function.
         /// For example setting the I2C1 data pin to use GPIO pin 17.
         /// </remarks>
-        /// <param name="pin"></param>
-        /// <param name="value"></param>
+        /// <param name="pin">The pin number to set against function.</param>
+        /// <param name="value">The device function to be assigned the pin.</param>
         public static void SetPinFunction(int pin, DeviceFunction value)
         {
-            GpioPin gpioPin = GpioController.GetDefault().OpenPin(pin);
-            gpioPin.SetAlternateFunction((int)value);
-            gpioPin.Dispose();
+            NativeSetPinFunction(pin, (int)value);
         }
+
+        /// <summary>
+        /// Returns the current pin number used by a device function.
+        /// </summary>
+        /// <param name="function"></param>
+        /// <returns>The pin number used by device function. If value is -1 then pins is not assigned.</returns>
+        public static int GetFunctionPin(DeviceFunction function)
+        {
+ 
+            return NativeGetPinFunction((int)function); ;
+        }
+
+        #region Native Calls
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        private extern static void NativeSetPinFunction(int pin, int function);
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        private extern static int NativeGetPinFunction(int function);
+        #endregion
     }
 }
