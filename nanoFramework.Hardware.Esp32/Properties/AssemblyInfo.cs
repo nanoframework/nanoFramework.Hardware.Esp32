f936accee6494fc85aaf9f2be774648eec4c1aed
José Simões <jose.simoes@eclo.solutions>
2023-02-10 17:21:46 +0000

    Bump AssemblyNativeVersion to 100.0.8.0

--- nanoFramework.Hardware.Esp32/Properties/AssemblyInfo.cs
+++ nanoFramework.Hardware.Esp32/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Runtime.InteropServices;
 
 ////////////////////////////////////////////////////////////////
 // update this whenever the native assembly signature changes //
-[assembly: AssemblyNativeVersion("100.0.7.3")]
+[assembly: AssemblyNativeVersion("100.0.8.0")]
 ////////////////////////////////////////////////////////////////
 
 // Setting ComVisible to false makes the types in this assembly not visible 
